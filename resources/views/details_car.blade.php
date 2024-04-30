0a917d288236f2327d8a31a53aba006413310c20
Paulo <paulo@cmprn.local>
2024-04-30 13:17:06 -0300

    New route

---
+++ resources/views/details_car.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Legendary Motorsport</title>
+</head>
+<body>
+    <header>
+        <a href="/">Legendary Motorsport</a>
+    </header>
+    <H1>Detalhes dos anúncio</H1>
+    <p> Página de detalhes dos anúncios dos carros</p>
+ </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/details_car', function () {
+    return view('details_car');
+});
