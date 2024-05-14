b3a41b096a33f730e1b1089e017790cceafb344e
Paulo <paulo@cmprn.local>
2024-05-14 13:44:12 -0300

    update layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,74 +6,100 @@
         <div class="container">
             <header>
                 <div class="row">
-                    <div class="col-xl-8">
+                    <div class="col-12">
                         <h1 class="text-center mt-3">Legendary Motorsport</h1>
                     </div>
                 </div>
+            <div class="row text-end">
+                <div class="col">
+                <button type="button" class="btn btn-danger btn-lg rounded-1">Em destaque</button>
+            </div>
+            </div>
             </header>
         </div>
     </section>
     <section id="content">
         <div class="container text-center">
-            <div class="row row-cols-3">
+            <div class="row py-3">
+        <div class="col">
+            <button type="button" class="btn btn-danger btn-lg rounded-1">Em destaque</button>
+        </div>
+        <div class="col">
+            <button type="button" class="btn btn-danger btn-lg rounded-1">2 PORTAS</button>
+        </div>
+        <div class="col">
+            <button type="button" class="btn btn-danger btn-lg rounded-1">4 PORTAS</button>
+        </div>
+        <div class="col">
+            <button type="button" class="btn btn-danger btn-lg rounded-1">ESPECIAL</button>
+        </div>
+        
+            </div>
+            <div class="row">
                 <div class="col">
                     <!-- card -->
-                    <div class="card rounded-0" style="width:18rem;">
-                        <img src="https://place-hold.it/200x60" class="card-img-top rounded-0" alt="...">
+                    <div class="card text-end rounded-0" style="width:25rem;">
+                        <img src="https://place-hold.it/200x100" class="card-img-top rounded-0" alt="...">
                         <div class="card-body">
                             <p class="card-text">Esto é um anúncios lkmijopi</p>
+                            <a href="#" class="btn btn-danger">Go somewhere</a>
                         </div>
                     </div>
                     <!-- /card -->
                 </div>
                 <div class="col">
                     <!-- card -->
-                    <div class="card rounded-0" style="width:18rem;">
-                        <img src="https://place-hold.it/200x60" class="card-img-top rounded-0" alt="...">
+                    <div class="card text-end rounded-0" style="width:25rem;">
+                        <img src="https://place-hold.it/200x100" class="card-img-top rounded-0" alt="...">
                         <div class="card-body">
                             <p class="card-text">Esto é um anúncios lkmijopi</p>
+                            <a href="#" class="btn btn-danger">Go somewhere</a>
                         </div>
                     </div>
                     <!-- /card -->
                 </div>
                 <div class="col">
                     <!-- card -->
-                    <div class="card rounded-0" style="width:18rem;">
-                        <img src="https://place-hold.it/200x60" class="card-img-top rounded-0" alt="...">
+                    <div class="card text-end rounded-0" style="width:25rem;">
+                        <img src="https://place-hold.it/200x100" class="card-img-top rounded-0" alt="...">
                         <div class="card-body">
                             <p class="card-text">Esto é um anúncios lkmijopi</p>
+                            <a href="#" class="btn btn-danger">Go somewhere</a>
                         </div>
                     </div>
                 </div>
                 <!-- /card -->
             </div>
-            <div class="row row-cols-3">
+            <div class="row mt-3">
                 <div class="col">
                     <!-- card -->
-                    <div class="card rounded-0" style="width:18rem;">
-                        <img src="https://place-hold.it/200x60" class="card-img-top rounded-0" alt="...">
+                    <div class="card text-end rounded-0" style="width:25rem;">
+                        <img src="https://place-hold.it/200x100" class="card-img-top rounded-0" alt="...">
                         <div class="card-body">
                             <p class="card-text">Esto é um anúncios lkmijopi</p>
+                            <a href="#" class="btn btn-danger">Go somewhere</a>
                         </div>
                     </div>
                     <!-- /card -->
                 </div>
                 <div class="col">
                     <!-- card -->
-                    <div class="card rounded-0" style="width:18rem;">
-                        <img src="https://place-hold.it/200x60" class="card-img-top rounded-0" alt="...">
+                    <div class="card text-end rounded-0" style="width:25rem;">
+                        <img src="https://place-hold.it/200x100" class="card-img-top rounded-0" alt="...">
                         <div class="card-body">
                             <p class="card-text">Esto é um anúncios lkmijopi</p>
+                            <a href="#" class="btn btn-danger">Go somewhere</a>
                         </div>
                     </div>
                     <!-- /card -->
                 </div>
                 <div class="col">
                     <!-- card -->
-                    <div class="card rounded-0" style="width:18rem;">
-                        <img src="https://place-hold.it/200x60" class="card-img-top rounded-0" alt="...">
+                    <div class="card text-end rounded-0" style="width:25rem;">
+                        <img src="https://place-hold.it/200x100" class="card-img-top rounded-0" alt="...">
                         <div class="card-body">
                             <p class="card-text">Esto é um anúncios lkmijopi</p>
+                            <a href="#" class="btn btn-danger">Go somewhere</a>
                         </div>
                     </div>
                     <!-- card -->
